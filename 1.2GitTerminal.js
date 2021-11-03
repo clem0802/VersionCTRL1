6c134870b37ddaee602862fb4e65c17a9ab3288c
clem0802 <88548513+clem0802@users.noreply.github.com>
2021-11-03 20:48:34 +0100

    Create 1.2GitTerminal.js

---
+++ 1.2GitTerminal.js
@@ -0,0 +1,81 @@
+// 2021.11.03 FROM (MIMO) 
+/*----------------------------------*/
+/*----------------------------------*/ 
+//! GIT + TERMINAL
+// the "pterminal" is a tool that gives commands to the computer
+// it is available on all computers
+// we give commands to GIT via the terminal
+
+
+/*----------------------------------*/
+/*----------------------------------*/ (I)
+// the terminal open at the "Documents" location of the computer
+// for the user named "alex"   
+//?? (TERMINAL)------------------ 
+alex:Documents alex$
+
+// the terminal open at the "Desktop" location of the computer
+// for the user named "isaac"   
+//?? (TERMINAL)------------------ 
+isaac:Desktop isaac$
+
+
+
+
+
+
+/*----------------------------------*/
+/*----------------------------------*/ (II)
+//! CD
+// we can navigate to different locations using the "cd"
+// after our "cd" command, we type the name fo the folder we want to navigate to
+// let's navigate to our "Desktop"
+// which stands for //!   CHANGE DIRECTORY
+//?? (TERMINAL)------------------ 
+alex:Desktop alex$ cd Desktop
+
+
+
+
+
+/*----------------------------------*/
+/*----------------------------------*/ (III)
+//! MKDIR
+// use this command to CREATE A NEW FOLDER
+//?? (TERMINAL)------------------ 
+alex:Desktop alex$ mkdir myproject
+
+
+
+
+/*----------------------------------*/
+/*----------------------------------*/ (IV)
+//! cd // mkdir
+// use this command to CREATE A NEW FOLDER
+//?? (TERMINAL)------------------ 
+alex:Desktop alex$ mkdir myproject
+alex:Desktop alex$ cd myproject/
+
+
+
+
+/*----------------------------------*/
+/*----------------------------------*/ (V)
+//! START AT ROOT FOLDER
+// to add our project to version control to start trackng changes and working with others
+// we start at the ROOT FOLDER
+//?? (TERMINAL)------------------ 
+alex:Desktop alex$ mkdir myproject
+
+
+
+
+/*----------------------------------*/
+/*----------------------------------*/ (VI)
+//! GIT INIT
+// to INITIALIZE a new GIT REPO
+// once we are at tour root folder, we can use GIT INIT to create a new GIT "repository"
+// or "repo" for short
+// we have now used "git init" to create a GIT repository in our "myproject" folder
+//?? (TERMINAL)------------------ 
+alex:Desktop alex$ mkdir myproject
