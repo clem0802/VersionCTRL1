813e50e4767056683136e550ebfffa538d75d69a
clem0802 <88548513+clem0802@users.noreply.github.com>
2021-11-03 23:28:26 +0100

    Create 1.6EXO.js

---
+++ 1.6EXO.js
@@ -0,0 +1,18 @@
+// 2021.11.03 FROM (M) 
+/*----------------------------------*/
+/*----------------------------------*/ 
+//! GTI EXO
+
+
+/*----------------------------------*/
+/*----------------------------------*/ EXO (I)
+//! GIT LOG
+//?? (TERMINAL)------------------ 
+// create a new directory called "myproject"
+lee:Desktop lee$ mkdir myproject
+
+// access it with "cd"
+lee:Desktop lee$ cd myproject
+
+// and use "git init" to initialize a new GIT repository inside this directory
+lee:Desktop lee$ git init 
